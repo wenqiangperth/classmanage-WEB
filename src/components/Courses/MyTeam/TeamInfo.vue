6f0783c16021544f0db07991808cbe4132309fc0
方波 <1768846318@qq.com>
2018-12-12 09:47:55 +0800

    666

--- src/components/Courses/MyTeam/TeamInfo.vue
+++ src/components/Courses/MyTeam/TeamInfo.vue
@@ -22,13 +22,37 @@
         </div>
       </header>
 
-      <el-collapse v-model="activeName" accordion>
-        <el-collapse-item title="hello" name="1">
-          <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-          <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+      <el-collapse v-model="activeName" class="team" accordion>
+        <el-collapse-item v-for="item in items">
+          <template slot="title">
+            <div style="font-size: 17px"><i class="header-icon el-icon-search"></i>
+               {{item.title}}   {{item.name}}</div>
+          </template>
+          <div style="text-align: left;font-size: 16px;font-family: 幼圆">
+            <div style="background-color: #66cccc;font-weight: bold">组长：{{item.leader.Na}}——{{item.leader.No}}</div>
+            <div v-for="mem in item.member">组员：{{mem.Na}}——{{mem.No}}</div>
+          </div>
         </el-collapse-item>
       </el-collapse>
 
+      <div class="pic"><img style="border-radius: 50px" src="../../../assets/1.jpg"></div>
+
+      <el-collapse v-model="activeName2">
+        <el-collapse-item >
+          <template slot="title">
+            <div style="font-size: 17px"><i class="header-icon el-icon-search"></i>
+              未组队学生</div>
+          </template>
+          <div v-for="ren in Unteam" style="text-align: left;font-size: 16px;font-family: 幼圆">
+            {{ren.Na}}——{{ren.No}}
+            <div class="line"/>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+
+      <div class="login-input">
+        <a >创建小组</a>
+      </div>
     </div>
 </template>
 
@@ -38,7 +62,41 @@
       data(){
         return{
           activeName: '1',
-          title: "hello"
+          activeName2: '2',
+          items:[{
+            title: '1-1',
+            name: 'HHHA',
+            leader: {Na:'直线',No:'24320162201111'},
+            member: [{Na:'赵四',No:'24320262209999'},
+              {Na:'五强',No:'24320162209980'}]
+          },{
+            title: '2-2',
+            name: 'W3C',
+            leader: {Na:'射线',No:'24320162202222'},
+            member: [{Na:'赵强',No:'24320262209999'},
+              {Na:'华融',No:'24320162209980'},
+              {Na:'cilil',No:'24320176668290'}]
+          },{
+            title: '3-2',
+            name: 'HelloWorld',
+            leader: {Na:'小宝',No:'24320162200000'},
+            member: [{Na:'三鹿',No:'24320262209119'},
+              {Na:'初年',No:'24320162200890'},
+              {Na:'lilzoo',No:'24320176663333'}]
+          }],
+          Unteam:[{
+            Na: '我是1号',
+            No: '11111111111'
+          },{
+            Na: '我是2号',
+            No: '22222222222'
+          },{
+            Na: '我是3号',
+            No: '33333333333'
+          },{
+            Na: '我是4号',
+            No: '44444444444'
+          }]
         };
       },
       methods:{
@@ -77,4 +135,30 @@
     transform: translateX(90%);
   }
 
+  .pic{
+    margin: 10px 0 10px 0;
+  }
+
+  .line{
+    height: 2px;
+    background-color: #66cccc;
+  }
+
+  .login-input{
+    margin:40px 30px 15px 30px;
+    border:1px solid #66CCCC;
+    border-radius:25px;
+  }
+
+  .login-input a{
+    display: block;
+    width: 100%;
+    text-align: center;
+    line-height: 40px;
+    color: #fff;
+    background-color: #66CCCC;
+    font-size: 17px;
+    letter-spacing: 5px;
+    border-radius: 25px;
+  }
 </style>
